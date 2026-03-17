180ce889baebf6666c504d290adbdf4848064b65
Diffblue CI <ci@diffblue.com>
2026-03-17 02:18:56 +0000

    test: Add generated tests for net.finmath.smartcontract.valuation.oracle.simulated.GeometricBrownianMotionOracle
    - <init>
    - <init>
    - <init>
    - <init>
    - init
    - getValue

--- src/test/java/net/finmath/smartcontract/valuation/oracle/GeometricBrownianMotionOracleTest.java
+++ src/test/java/net/finmath/smartcontract/valuation/oracle/GeometricBrownianMotionOracleTest.java
@@ -7,9 +7,13 @@
 package net.finmath.smartcontract.valuation.oracle;
 
 import net.finmath.smartcontract.valuation.oracle.simulated.GeometricBrownianMotionOracle;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.time.TimeDiscretizationFromArray;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Christian Fries
@@ -32,6 +36,96 @@ class GeometricBrownianMotionOracleTest {
 			System.out.println(time.toLocalDate() + "\t" + value);
 
 		}
+	}
+
+	@Test
+	void testDefaultConstructor() {
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle();
+		assertNotNull(oracle);
+	}
+
+	@Test
+	void testConstructorWithInitialTime() {
+		final LocalDateTime initialTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle(initialTime);
+		assertNotNull(oracle);
+	}
+
+	@Test
+	void testConstructorWithParameters() {
+		final LocalDateTime initialTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final double initialValue = 100.0;
+		final double timeHorizon = 10.0;
+		final double riskFreeRate = 0.05;
+		final double volatility = 0.20;
+		final int numberOfPaths = 500;
+
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle(
+				initialTime, initialValue, timeHorizon, riskFreeRate, volatility, numberOfPaths);
+		assertNotNull(oracle);
+	}
+
+	@Test
+	void testConstructorWithTimeDiscretization() {
+		final LocalDateTime initialTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final double initialValue = 100.0;
+		final double riskFreeRate = 0.05;
+		final double volatility = 0.20;
+		final int numberOfPaths = 500;
+		final TimeDiscretizationFromArray timeDiscretization = new TimeDiscretizationFromArray(0.0, 5.0, 0.1);
+
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle(
+				timeDiscretization, initialTime, initialValue, riskFreeRate, volatility, numberOfPaths);
+		assertNotNull(oracle);
+	}
+
+	@Test
+	void testGetValue() {
+		final LocalDateTime initialTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final LocalDateTime evaluationTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final LocalDateTime marketDataTime = LocalDateTime.of(2020, 6, 1, 0, 0);
+
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle(initialTime);
+		final RandomVariable value = oracle.getValue(evaluationTime, marketDataTime);
+
+		assertNotNull(value);
+		assertTrue(value.getAverage() > 0);
+	}
+
+	@Test
+	void testGetValueMultipleTimes() {
+		final LocalDateTime initialTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final LocalDateTime evaluationTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle(initialTime);
+
+		final RandomVariable value1 = oracle.getValue(evaluationTime, LocalDateTime.of(2020, 6, 1, 0, 0));
+		final RandomVariable value2 = oracle.getValue(evaluationTime, LocalDateTime.of(2021, 1, 1, 0, 0));
+
+		assertNotNull(value1);
+		assertNotNull(value2);
+		assertTrue(value1.getAverage() > 0);
+		assertTrue(value2.getAverage() > 0);
+	}
+
+	@Test
+	void testGetValueWithDifferentParameters() {
+		final LocalDateTime initialTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final double initialValue = 50.0;
+		final double timeHorizon = 20.0;
+		final double riskFreeRate = 0.03;
+		final double volatility = 0.15;
+		final int numberOfPaths = 1000;
+
+		final GeometricBrownianMotionOracle oracle = new GeometricBrownianMotionOracle(
+				initialTime, initialValue, timeHorizon, riskFreeRate, volatility, numberOfPaths);
+
+		final LocalDateTime evaluationTime = LocalDateTime.of(2020, 1, 1, 0, 0);
+		final LocalDateTime marketDataTime = LocalDateTime.of(2021, 1, 1, 0, 0);
+		final RandomVariable value = oracle.getValue(evaluationTime, marketDataTime);
+
+		assertNotNull(value);
+		assertTrue(value.getAverage() > 0);
 	}
 
 }
