b2d70d1f261e10f029d4ed151f9f0331b3344e96
Diffblue CI <ci@diffblue.com>
2026-04-02 19:13:15 +0000

    test: Add generated tests for net.finmath.smartcontract.valuation.service.websocket.WebSocketConfig
    - registerWebSocketHandlers
    - createWebSocketContainer

---
+++ src/test/java/net/finmath/smartcontract/valuation/service/WebSocketConfigTest.java
@@ -0,0 +1,43 @@
+package net.finmath.smartcontract.valuation.service;
+
+import net.finmath.smartcontract.valuation.service.websocket.WebSocketConfig;
+import net.finmath.smartcontract.valuation.service.websocket.handler.ValuationHandler;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class WebSocketConfigTest {
+
+	@Test
+	void testCreateWebSocketContainer() {
+		WebSocketConfig config = new WebSocketConfig();
+		ServletServerContainerFactoryBean container = config.createWebSocketContainer();
+
+		assertEquals(Integer.valueOf(1024 * 1024), container.getMaxTextMessageBufferSize());
+		assertEquals(Integer.valueOf(1024 * 1024), container.getMaxBinaryMessageBufferSize());
+	}
+
+	@Test
+	void testRegisterWebSocketHandlers() {
+		WebSocketHandlerRegistry registry = Mockito.mock(WebSocketHandlerRegistry.class);
+		WebSocketHandlerRegistration registration = Mockito.mock(WebSocketHandlerRegistration.class);
+
+		when(registry.addHandler(any(ValuationHandler.class), eq("/valuationfeed"))).thenReturn(registration);
+		when(registration.setAllowedOrigins("*")).thenReturn(registration);
+
+		WebSocketConfig config = new WebSocketConfig();
+		config.registerWebSocketHandlers(registry);
+
+		verify(registry).addHandler(any(ValuationHandler.class), eq("/valuationfeed"));
+		verify(registration).setAllowedOrigins("*");
+	}
+
+}
