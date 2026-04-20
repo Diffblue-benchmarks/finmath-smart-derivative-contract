f4265d2ee3853ec8b165f8b040a14ce577ada843
Christopher Hubert <christopher.hubert@diffblue.com>
2026-04-20 11:19:07 +0100

    test: Add generated tests for net.finmath.smartcontract.valuation.marketdata.utils.MarketDataCheck.checkMarketData(Lnet/finmath/smartcontract/model/MarketDataList;Lnet/finmath/smartcontract/product/xml/Smartderivativecontract;)Lnet/finmath/smartcontract/valuation/marketdata/utils/MarketDataErrors;

--- src/test/java/net/finmath/smartcontract/valuation/marketdata/utils/MarketDataCheckTest.java
+++ src/test/java/net/finmath/smartcontract/valuation/marketdata/utils/MarketDataCheckTest.java
@@ -24,6 +24,20 @@ class MarketDataCheckTest {
 
 
 	@Test
+	void testCheckMarketDataEmptyList() throws IOException {
+		final String fpml = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract.xml").readAllBytes(), StandardCharsets.UTF_8);
+		Smartderivativecontract sdc = SDCXMLParser.unmarshalXml(fpml, Smartderivativecontract.class);
+
+		MarketDataList emptyList = new MarketDataList();
+
+		MarketDataErrors errors = MarketDataCheck.checkMarketData(emptyList, sdc);
+		assertTrue(errors.hasErrors());
+		assertEquals("error in marketData service - no data generated", errors.getErrorMessage());
+		assertEquals(1, errors.getMissingDataPoints().size());
+		assertEquals("all, no data provided", errors.getMissingDataPoints().get(0));
+	}
+
+	@Test
 	void checkMarketData_negativeCase() throws IOException {
 		final String fpml = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract.xml").readAllBytes(), StandardCharsets.UTF_8);
 		final String md = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net/finmath/smartcontract/valuation/historicalMarketData/marketdata_2008-09-01.xml").readAllBytes(), StandardCharsets.UTF_8);
@@ -34,20 +48,6 @@ class MarketDataCheckTest {
 		MarketDataErrors errors = MarketDataCheck.checkMarketData(mdl, sdc);
 		assertTrue(errors.hasErrors());
 		assertEquals(14, errors.getMissingDataPoints().size());
-	}
-
-	@Disabled("check format, two fixings are not included in product xml")
-	@Test
-	void checkMarketData_positiveCase_newFormat() throws IOException {
-		final String fpml = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract.xml").readAllBytes(), StandardCharsets.UTF_8);
-		final String md = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net/finmath/smartcontract/valuation/client/settlement_testset_initial.xml").readAllBytes(), StandardCharsets.UTF_8);
-
-		Settlement settlement = SDCXMLParser.unmarshalXml(md, Settlement.class);
-		Smartderivativecontract sdc = SDCXMLParser.unmarshalXml(fpml, Smartderivativecontract.class);
-
-		MarketDataErrors errors = MarketDataCheck.checkMarketData(settlement.getMarketData(), sdc);
-		assertFalse(errors.hasErrors());
-		assertEquals(0, errors.getMissingDataPoints().size());
 	}
 
 	@Test
